859ce3c45d645698441553e80457e4566f7e0cca
Aike Gilabert Gámez <raulgilabert@proton.me>
2025-03-09 20:33:23 +0100

    Refactor (#7)
    * first part of refactor
    
    * little refactor on parser done

--- src/parser.c
+++ src/parser.c
@@ -55,30 +55,18 @@ void new_instruction(InstructionTableArray *tables_array, u_int64_t cycle, char 
 
     // Create a new instruction
     Instruction instr_new;
-    instr_new.qtty_stages = 0;
-    instr_new.mem_addr = NULL;
-    instr_new.instruction = NULL;
-    instr_new.stages = malloc(10 * sizeof(Stage));
-    instr_new.valid = true;
-    instr_new.finished = false;
+    init_Instruction(&instr_new);
     
     // Create table if instruction needs to be in a new one
     if (id_file/256 >= tables_array->qtty_tables) {
         // Resize the tables array if necessary
         if (id_file/256 >= tables_array->avail_tables) {
-            tables_array->avail_tables *= 2;
-            tables_array->tables = realloc(tables_array->tables,tables_array->avail_tables * sizeof(InstructionTable*));
-
-            for (int i = tables_array->qtty_tables; i < tables_array->avail_tables; i++) {
-                tables_array->tables[i] = NULL;
-            }
+            increase_InstructionTableArray(tables_array);
         }
 
         tables_array->tables[id_file/256] = malloc(sizeof(InstructionTable));
 
-        for (int i = 0; i < 256; i++) {
-            tables_array->tables[id_file/256]->content[i].valid = false;
-        }
+        init_InstructionTable(tables_array->tables[id_file/256]);
 
         tables_array->qtty_tables = id_file/256 + 1;
     }
@@ -125,8 +113,7 @@ void new_stage(InstructionTableArray *tables_array, u_int64_t cycle, char *line)
     }
 
     // Get the instruction
-    Instruction *instr = 
-        &tables_array->tables[instr_id/256]->content[instr_id%256];
+    Instruction *instr = &tables_array->tables[instr_id/256]->content[instr_id%256];
 
     // Resize the stages array if necessary
     if (instr->qtty_stages % 10 == 0) {
@@ -152,8 +139,7 @@ void end_stage(InstructionTableArray *tables_array, u_int64_t cycle, char *line)
     }
 
     // Get the instruction
-    Instruction *instr = 
-        &tables_array->tables[instr_id/256]->content[instr_id%256];
+    Instruction *instr = &tables_array->tables[instr_id/256]->content[instr_id%256];
 
     // Get the stage
     Stage *stage = NULL;
@@ -207,20 +193,8 @@ InstructionTableArray parse_file(char *file_name) {
 
     // Initialization of the tables array
     InstructionTableArray tables_array;
-    tables_array.qtty_tables = 1;
-    tables_array.avail_tables = 4;
-    tables_array.tables = malloc(4 * sizeof(InstructionTable*));
 
-    // Initialization of the first table    
-    tables_array.tables[0] = malloc(sizeof(InstructionTable));
-
-    for (int i = 0; i < 256; i++) {
-        tables_array.tables[0]->content[i].valid = false;
-    }
-
-    for (int i = 1; i < 4; i++) {
-        tables_array.tables[i] = NULL;
-    }
+    init_InstructionTableArray(&tables_array);
 
     u_int64_t cycle = 0;
 
