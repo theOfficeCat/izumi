72e40edcd89e3654845edc26296b5cc91a411fc6
Raúl Gilabert Gámez <raulgilabert@proton.me>
2025-03-24 00:19:09 +0100

    create panel and resize window

--- src/files.c
+++ src/files.c
@@ -1,29 +1,26 @@
 /*
  * This file is part of Izumi.
- * 
+ *
  * Izumi is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * Izumi is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
- * Izumi. If not, see <https://www.gnu.org/licenses/>. 
+ * Izumi. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include <dirent.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/stat.h>
 #include <stdbool.h>
 #include <limits.h>
 
 #include "files.h"
-#include "window.h"
 #include "data_structs.h"
 #include "parser.h"
 
