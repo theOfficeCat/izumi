16c19dfc9cfbccd4183da9b05bc48725267c2af5
Nara Díaz Viñolas <rdvdev2@gmail.com>
2025-07-28 23:28:06 +0200

    Separate filelists into .mk files

---
+++ src/libizumi/libizumi.mk
@@ -0,0 +1,9 @@
+libizumi_c_sources = \
+    data_structs.c \
+    finder.c \
+    parser.c
+
+libizumi_headers = \
+    include/izumi/data_structs.h \
+    include/izumi/finder.h \
+    include/izumi/parser.h
