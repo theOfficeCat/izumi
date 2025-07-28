f647022bca2a9a2b8677f0d7c91ae55da9b37a5c
Nara Díaz Viñolas <rdvdev2@gmail.com>
2025-07-28 23:28:06 +0200

    Separate filelists into .mk files

---
+++ src/izumi_tui/izumi_tui.mk
@@ -0,0 +1,17 @@
+izumi_c_sources = \
+    command_tree.c \
+    commands.c \
+    configure.c \
+    files.c \
+    interact.c \
+    main.c \
+    window.c
+
+izumi_headers = \
+    include/command_tree.h \
+    include/commands.h \
+    include/configure.h \
+    include/files.h \
+    include/interact.h \
+    include/system_curses.h \
+    include/window.h
